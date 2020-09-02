c8c5ff3d949be8bb2bc8303e8401fceece1b5a93
diana.estrada <diana.estrada.hdez@gmail.com>
2020-09-02 17:49:26 -0500

    cobol

--- calc.cbl
+++ calc.cbl
@@ -1,48 +1,5 @@
 IDENTIFICATION DIVISION.
-       PROGRAM-ID. COBOLCalc.
-	   DATA DIVISION.
-	   FILE SECTION.
-       WORKING-STORAGE SECTION.
-           01 num1 PIC 9(4).
-           01 num2 PIC 9(4).
-           01 total PIC 9(8).
-           01 opt PIC X.
-       PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-           DISPLAY "COBOL Calculator"
-           DISPLAY " ".
-           DISPLAY "Use enter key after each element: "
-           DISPLAY "Number:"
-           ACCEPT total
-
-           PERFORM UNTIL opt = "="
-
-               EVALUATE TRUE
-                   WHEN opt = "="
-                       DISPLAY "Total:"
-                   WHEN opt = "+"
-                       COMPUTE total = total + num1
-                   WHEN opt = "-"
-                       COMPUTE total = total - num1
-                   WHEN opt = "*"
-                       COMPUTE total = total * num1
-                   WHEN opt = "/"
-                       COMPUTE total = total / num1
-                   WHEN OTHER
-                       DISPLAY "Invaild opt"
-               END-EVALUATE
-
-               DISPLAY "Opt:"
-               ACCEPT opt
-
-               IF opt = "+" OR opt = "-" OR opt = "*" OR opt = "/"
-                   DISPLAY "Number:"
-                   ACCEPT num1
-               END-IF
-
-           END-PERFORM
-
-           DISPLAY total.
-
-       STOP RUN.
-       END PROGRAM COBOLCalc.
+PROGRAM-ID. HELLO-WORLD.
+PROCEDURE DIVISION.
+DISPLAY "Hello world DIANA ESTRADA!".
+STOP RUN.
